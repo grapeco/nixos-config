97a8c51e87b2a17bd98b2b9c3b475956f9903281
BitterLemon-git <mihajlovs928@gmail.com>
2025-03-29 17:09:24 +0300

    Add kitty

---
+++ home-manager/modules/kitty.nix
@@ -0,0 +1,10 @@
+{}: {
+  programs.kitty = lib.mkForce {
+    enable = true;
+    settings = {
+      confirm_os_window_close = 0;
+      background_opacity = "0.5";
+      background_blur = 5;  
+    };
+  };
+}
