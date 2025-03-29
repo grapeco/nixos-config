31294a172498316f5eb61296a232d51a4049a8f8
BitterLemon-git <mihajlovs928@gmail.com>
2025-03-29 17:17:11 +0300

    Add kitty

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -5,7 +5,12 @@
       confirm_os_window_close = 0;
       background_opacity = "0.5";
       background_blur = 5;  
+
       font_size = 12;
+      font_family = "Operator Mono Book";
+      bold_font = "Operator Mono Medium";
+      italic_font = "Operator Mono Book Italic";
+      bold_italic_font = "Operator Mono Medium Italic";
     };
   };
 }
