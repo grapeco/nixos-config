d4058057d6c6eead7563c2f9b0a77c431b3f2a85
BitterLemon-git <mihajlovs928@gmail.com>
2025-03-29 21:42:42 +0300

    Delete opacity and blur in kitty

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -3,10 +3,8 @@
     enable = true;
     settings = {
       confirm_os_window_close = 0;
-      background_opacity = "0.5";
-      background_blur = 5;  
 
-      font_size = 12;
+      font_size = 13;
       font_family = "Operator Mono Book";
       bold_font = "Operator Mono Medium";
       italic_font = "Operator Mono Book Italic";
