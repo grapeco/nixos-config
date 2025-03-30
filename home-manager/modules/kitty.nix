00d4ae96747108b269c08b07361fc225d7618fb6
BitterLemon-git <mihajlovs928@gmail.com>
2025-03-30 08:31:18 +0300

    Change cursor

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -2,16 +2,13 @@
   programs.kitty = lib.mkForce {
     enable = true;
     settings = {
-      confirm_os_window_close = 0;
-      background_opacity = "0.5";
-      background_blur = 5;  
+      confirm_os_window_close = 0; 
 
       font_size = 12;
       font_family = "Operator Mono Book";
       bold_font = "Operator Mono Medium";
       italic_font = "Operator Mono Book Italic";
       bold_italic_font = "Operator Mono Medium Italic";
-      
     };
   };
 }
