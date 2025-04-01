17075e6c744e133b29ff94bd9d68fd0bd701ac7b
BitterLemon-git <mihajlovs928@gmail.com>
2025-04-01 16:50:25 +0300

    Fix the vscode settings

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -2,6 +2,7 @@
   
   programs.vscode = {
     enable = true;
+    mutableExtensionsDir = false;
     extensions = with pkgs.vscode-extensions; [
       rust-lang.rust-analyzer
       bbenoist.nix
@@ -11,24 +12,9 @@
       # harg.iceberg(imperative)
     ];
     userSettings = {
-      "chat.editor.fontFamily" = "JetBrains Mono";
-      "chat.editor.fontSize" = 17.333333333333332;
-      "debug.console.fontFamily" = "JetBrains Mono";
-      "debug.console.fontSize" = 17.333333333333332;
-      "editor.fontFamily" = "JetBrains Mono";
-      "editor.fontSize" = 17.333333333333332;
-      "editor.inlayHints.fontFamily" = "JetBrains Mono";
-      "editor.inlineSuggest.fontFamily" = "JetBrains Mono";
-      "editor.minimap.sectionHeaderFontSize" = 11.142857142857142;
-      "markdown.preview.fontFamily" = "Noto Sans";
-      "markdown.preview.fontSize" = 17.333333333333332;
-      "scm.inputFontFamily" = "JetBrains Mono";
-      "scm.inputFontSize" = 16.095238095238095;
-      "screencastMode.fontSize" = 69.33333333333333;
-      "terminal.integrated.fontSize" = 17.333333333333332;
-      "workbench.colorTheme" = "Stylix";
-      "workbench.iconTheme" = "catppuccin-mocha";
+      "window.titleBarStyle" = "custom";
       "files.autoSave" = "afterDelay";
+      "workbench.iconTheme" = "catppuccin-mocha";
     };
   };
 }
