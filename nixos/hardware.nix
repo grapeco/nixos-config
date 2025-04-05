49d39ce5cb2f0501b08672b94933f44302604b90
BitterLemon-git <mihajlovs928@gmail.com>
2025-04-05 10:36:01 +0300

    More slicing config + steam

---
+++ nixos/hardware.nix
@@ -0,0 +1,13 @@
+{
+  hardware.enableAllFirmware = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  };
+}
