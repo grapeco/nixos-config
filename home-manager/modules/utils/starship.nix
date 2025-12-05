fa5a6f13c0475ba56b05ae7737ab287c3bfe8f2d
grapeco <mihajlovs928@gmail.com>
2025-12-06 02:15:50 +0300

    Upgrade starship config

--- home-manager/modules/utils/starship.nix
+++ home-manager/modules/utils/starship.nix
@@ -1,25 +1,26 @@
-{ ... }: {
+{ config, ... }:
+
+with config.lib.stylix.colors.withHashtag;
+{
   programs.starship = {
     enable = true;
     settings = {
-      # right_format = "$cmd_duration";
       directory = {
-        format = "[ ](bold #89b4fa)[ $path ]($style)";
-        style = "bold #b4befe";
+        format = "[ ](bold ${base0D})[ $path ]($style)";
+        style = "bold ${base0D}";
       };
 
       character = {
-        success_symbol = "[ ](bold #89b4fa)[ ➜](bold green)";
-        error_symbol = "[ ](bold #89b4fa)[ ➜](bold red)";
+        success_symbol = "[ ](bold ${base0D})[ ➜](bold green)";
+        error_symbol = "[ ](bold ${base0D})[ ➜](bold red)";
       };
 
       cmd_duration = {
         format = "[󰔛 $duration]($style)";
-        disabled = false;
-        style = "bg:none fg:#f9e2af";
+        style = "bg:none fg:${base0A}";
         show_notifications = false;
         min_time_to_notify = 60000;
-      };        
-    }; 
+      };
+    };
   };
 }
