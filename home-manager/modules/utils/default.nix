8cf65db39e4b5fe2f7b5c7520a3348cff050e027
grapeco <mihajlovs928@gmail.com>
2025-12-31 16:48:16 +0300

    Added starship and gruvbox for test

--- home-manager/modules/utils/default.nix
+++ home-manager/modules/utils/default.nix
@@ -1,14 +1,11 @@
 {
   imports = [
-    # ./cava.nix
     ./direnv.nix
     ./eza.nix
     ./fish.nix
     ./gammastep.nix
-    # ./git.nix
     ./terminal.nix
     ./ssh.nix
-    ./starship.nix
     ./stylix.nix
   ];
 }
