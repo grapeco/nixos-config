9871e63a21802817d4f87410e4734531f1239ee8
grapeco <mihajlovs928@gmail.com>
2026-09-03 18:40:37 +0300

    Changing TLP settings

--- hosts/laptop/modules/battery.nix
+++ hosts/laptop/modules/battery.nix
@@ -2,11 +2,8 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      
-      START_CHARGE_THRESH_BAT0 = 95;
-      STOP_CHARGE_THRESH_BAT0 = 100;
+      TLP_PROFILE_AC = "PRF";
+      TLP_PROFILE_BAT = "SAV";
     };
   };
   
